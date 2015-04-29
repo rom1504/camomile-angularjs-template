abeff253a36302cc818920cc23282fa451ccd4de
Herve Bredin <bredin@limsi.fr>
2015-04-29 14:05:52 +0200

    fix: dummy default server

--- js/config.js
+++ js/config.js
@@ -1,16 +1,15 @@
-
 // This configuration file is used by the Camomile service
-// to know the URL of the Camomile REST API
+// to know the URL of the Camomile REST API, see app.js
 
 angular.module('camomileApp.production', [])
-  .constant('camomileConfig', {
-    backend: 'https://camomile.fr/api'
-  });
+    .constant('camomileConfig', {
+        backend: 'http://www.example.com/api'
+    });
 
 angular.module('camomileApp.development', [])
-  .constant('camomileConfig', {
-    backend: 'https://dev.camomile.fr/api'
-  });
+    .constant('camomileConfig', {
+        backend: 'http://localhost:3000'
+    });
 
 // More info on configuration files 
 // at http://www.ng-newsletter.com/advent2013/#!/day/5
