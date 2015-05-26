cdda962f99464b7542f75a820c2e2b71e11c9faa
Herve Bredin <bredin@limsi.fr>
2015-05-26 08:28:08 +0200

    improve: config.js links to localhost

--- js/config.js
+++ js/config.js
@@ -2,14 +2,14 @@
 // to know the URL of the Camomile REST API, see app.js
 
 angular.module('camomileApp.production', [])
-    .constant('camomileConfig', {
-        backend: 'http://www.example.com/api'
-    });
+	.constant('camomileConfig', {
+		backend: 'http://localhost:3000'
+	});
 
-angular.module('camomileApp.development', [])
-    .constant('camomileConfig', {
-        backend: 'http://localhost:3000'
-    });
+// angular.module('camomileApp.development', [])
+// 	.constant('camomileConfig', {
+// 		backend: 'http://localhost:3000'
+// 	});
 
 // More info on configuration files 
 // at http://www.ng-newsletter.com/advent2013/#!/day/5
